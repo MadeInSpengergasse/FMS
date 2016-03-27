d830f99053f319f45ba49ac1a7d8c92aa9f850a0
z3ntu <luca.emanuel.weiss@gmail.com>
2016-03-27 18:34:18 +0200

    copy from master

---
+++ FMS/FarmerManagement_CREATE.sql
@@ -0,0 +1,119 @@
+USE "FMS";
+
+CREATE TABLE f_farmer (
+  f_id        INT  NOT NULL,
+  f_firstname TEXT NOT NULL,
+  f_lastname  TEXT NOT NULL,
+  f_address   TEXT NOT NULL,
+  PRIMARY KEY (f_id)
+);
+
+CREATE TABLE pr_product (
+  pr_id    INT  NOT NULL,
+  pr_type  TEXT NOT NULL,
+  pr_class INT  NULL,
+  PRIMARY KEY (pr_id)
+);
+
+CREATE TABLE a_animal (
+  a_id             INT  NOT NULL,
+  a_animal         TEXT NULL,
+  a_age            REAL NOT NULL,
+  a_weight         REAL NOT NULL,
+  a_classification INT  NOT NULL,
+  f_farmer_f_id    INT  NOT NULL,
+  a_pr_product     INT  NULL,
+  PRIMARY KEY (a_id),
+  CONSTRAINT fk_a_animal_f_farmer1
+  FOREIGN KEY (f_farmer_f_id)
+  REFERENCES f_farmer (f_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_a_animal_pr_product1
+  FOREIGN KEY (a_pr_product)
+  REFERENCES pr_product (pr_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE TABLE c_corn (
+  c_id         INT  NOT NULL,
+  c_type       TEXT NOT NULL,
+  c_class      INT  NULL,
+  c_dour       INT  NOT NULL,
+  c_pr_product INT  NULL,
+  PRIMARY KEY (c_id),
+  CONSTRAINT fk_c_corn_pr_product1
+  FOREIGN KEY (c_pr_product)
+  REFERENCES pr_product (pr_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE TABLE p_property (
+  p_id          INT  NOT NULL,
+  p_name        TEXT NOT NULL,
+  p_description TEXT NULL,
+  p_f_farmer    INT  NOT NULL,
+  p_area        REAL NOT NULL,
+  p_c_corn      INT  NULL,
+  PRIMARY KEY (p_id),
+
+  CONSTRAINT fk_p_property_f_farmer1
+  FOREIGN KEY (p_f_farmer)
+  REFERENCES f_farmer (f_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_p_property_c_corn1
+  FOREIGN KEY (p_c_corn)
+  REFERENCES c_corn (c_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE TABLE cf_farmer_has_corn (
+  cf_f_farmer INT NOT NULL,
+  cf_c_corn   INT NOT NULL,
+  PRIMARY KEY (cf_f_farmer, cf_c_corn),
+  CONSTRAINT fk_f_farmer_has_c_corn_f_farmer1
+  FOREIGN KEY (cf_f_farmer)
+  REFERENCES f_farmer (f_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_f_farmer_has_c_corn_c_corn1
+  FOREIGN KEY (cf_c_corn)
+  REFERENCES c_corn (c_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE TABLE cu_customer (
+  cu_id        INT  NOT NULL,
+  cu_lastname  TEXT NOT NULL,
+  cu_firstname TEXT NOT NULL,
+  cu_address   TEXT NULL,
+  PRIMARY KEY (cu_id)
+);
+
+CREATE TABLE fpr_farmer_sells_product (
+  fpr_f_farmer      INT  NOT NULL,
+  fpr_pr_product    INT  NOT NULL,
+  fpr_price         REAL NOT NULL,
+  cu_customer_cu_id INT  NOT NULL,
+  PRIMARY KEY (fpr_f_farmer, fpr_pr_product),
+  CONSTRAINT fk_f_farmer_has_pr_product_f_farmer1
+  FOREIGN KEY (fpr_f_farmer)
+  REFERENCES f_farmer (f_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_f_farmer_has_pr_product_pr_product1
+  FOREIGN KEY (fpr_pr_product)
+  REFERENCES pr_product (pr_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT fk_fpr_farmer_sells_product_cu_customer1
+  FOREIGN KEY (cu_customer_cu_id)
+  REFERENCES cu_customer (cu_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # FarmManagementSoftware
 
-FMS is a C# WPF Project made in School by @lukas8971 & @z3ntu.a
+FMS is a **C# WPF Project** made in School by [@lukas8971](https://github.com/lukas8971) & [@z3ntu](https://github.com/z3ntu).
